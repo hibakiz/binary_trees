f87bd6fd6c4f1bb8a7fb020a687fdac4775affdb
hibakiz <hbosalah98sd@gmail.com>
2024-01-30 18:19:30 +0300

    task 1 tany

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,7 +9,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *old_left;
 
-		new = malloc(sizeof(binary_tree_t));
+	if (parent == NULL)
+		return(NULL);
+
+	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = value;
