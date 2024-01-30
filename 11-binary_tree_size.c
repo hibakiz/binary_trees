f6f14f73c090d8695cb8ec7f4f6b06555071f416
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-01-30 09:39:02 +0400

    size of tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,7 +10,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	size_t r = binary_tree_size(tree->right);
 
-	return (1 + binary_tree_size(tree->left) + r);
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
 }
