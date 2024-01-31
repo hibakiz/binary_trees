1e1696e30254250cf1e013ad5c2c9208a14d51bb
hibakiz <hbosalah98sd@gmail.com>
2024-01-31 22:13:17 +0300

    error fix

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,7 +6,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node && node->parent->parent)
+	if ((node && node->parent) && node->parent->parent)
 	{
 		if (node->parent == node->parent->parent->right)
 			return (node->parent->parent->left);
