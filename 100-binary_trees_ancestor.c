14f41c942977a830ab1877b526f84cf02ad9b672
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-01 07:36:52 +0400

    ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,46 @@
+#include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - This finds the lowest ancestor of two nodes
+ * @first: pointer to first node
+ * @second: pointer to second
+ *
+ * Return: Pointer to the lowest common ancestor,
+ * otherwise NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+{
+	binary_tree_t *anc;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+
+	anc = (binary_tree_t *)first;
+
+	while (anc)
+	{
+		if (binary_tree_help(anc, second))
+			return (anc);
+		anc = anc->parent;
+	}
+
+	return (NULL);
+}
+
+/**
+ * binary_tree_help - Checks if is an ancestor
+ * @anc: pointer to ancestor node
+ * @node: pointer to descendant node
+ *
+ * Return: 1 if ancestor, 0 otherwise
+ */
+int binary_tree_help(const binary_tree_t *anc, const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (anc == node)
+		return (1);
+
+	return (binary_tree_help(anc, node->parent));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -51,5 +51,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 size_t get_height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+int binary_tree_help(const binary_tree_t *anc, const binary_tree_t *node);
 
 #endif
