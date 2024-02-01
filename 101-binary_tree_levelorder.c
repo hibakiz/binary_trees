fdfdc5ba4937d33bc1622eda05f827788d86c585
hibakiz <hbosalah98sd@gmail.com>
2024-02-01 16:39:35 +0300

    .h task20

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -15,7 +15,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	if (!tree || !func)
 		return;
 
-	max_level = binary_tree_height(tree) + 1;
+	max_level = binary_tree_height2(tree) + 1;
 
 	for (level = 1; level <= max_level; level++)
 		levelorder_help(tree, func, level);
@@ -42,7 +42,7 @@ void levelorder_help(const binary_tree_t *tree,
 }
 
 /**
- * binary_tree_height - This measures the height of a binary tree.
+ * binary_tree_height2 - This measures the height of a binary tree.
  * @tree: pointer to the root node of the tree to measure the height.
  *
  * Return: hight otherwise,  0 if it is NULL.
@@ -53,8 +53,8 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 		return (0);
 
 	size_t l, r;
-	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	l = tree->left ? 1 + binary_tree_height2(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height2(tree->right) : 0;
 
 	return (max(l, r));
 }
