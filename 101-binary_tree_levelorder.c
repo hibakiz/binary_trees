6c2dfe8399954477775c15f5da06e9d5b1eb7488
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-01 08:08:58 +0400

    LEVELORDER

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -2,36 +2,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
 /**
- * binary_tree_levelorder - This for using level-order traversal
- * @tree: Pointer to the root node
- * @func: Pointer to a function to call
+ * binary_tree_levelorder - Performs level-order traversal of a binary tree
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL || !func)
+	size_t level, max_level;
+
+	if (!tree || !func)
 		return;
 
-	binary_tree_t **queue = malloc(sizeof(binary_tree_t *) * 1024);
+	max_level = binary_tree_height(tree) + 1;
 
-	if (!queue)
-		return;
+	for (level = 1; level <= max_level; level++)
+		levelorder_help(tree, func, level);
+}
 
-	int a = 0, b = 0;
-	binary_tree_t *current;
-
-	queue[b++] = (binary_tree_t *)tree;
-
-	while (a < b)
+/**
+ * btlo_helper - Helpe levelorder func
+ * @tree: Pointer to the current node
+ * @func: Pointer to a function to call for each node
+ * @level: Current level in tree
+ */
+void levelorder_help(const binary_tree_t *tree,
+					 void (*func)(int), size_t level)
+{
+	if (level == 1)
+		func(tree->n);
+	else
 	{
-		current = queue[a++];
-		func(current->n);
-
-		if (current->left)
-			queue[b++] = current->left;
-		if (current->right)
-			queue[b++] = current->right;
+		if (tree->left)
+			levelorder_help(tree->left, func, level - 1);
+		if (tree->right)
+			levelorder_help(tree->right, func, level - 1);
 	}
+}
 
-	free(queue);
+/**
+ * binary_tree_height - This measures the height of a binary tree.
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: hight otherwise,  0 if it is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	size_t l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	size_t r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (max(l, r));
+}
+
+/**
+ * max - Calculates the maximum
+ * @l: First number
+ * @r: Second number
+ *
+ * Return: maximum
+ */
+size_t max(size_t l, size_t r)
+{
+	return (l > r ? l : r);
 }
