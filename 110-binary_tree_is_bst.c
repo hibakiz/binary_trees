ce525a5b549f7d57ccbf7e54e7493edb16e6d5e5
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-01 19:43:41 +0400

    bst

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * best_helper -  recursively check if  is a valid BST
+ * @tree: A pointer to the root node
+ * @min: The minimum value
+ * @max: The maximum value
+ * Return: true if the subtree rooted at tree is a valid BST, otherwise false
+ */
+bool best_helper(const binary_tree_t *tree, int min, int max)
+{
+    if (tree == NULL)
+        return true;
+
+    if (tree->n < min || tree->n > max)
+        return false;
+
+    return best_helper(tree->left, min, tree->n - 1) &&
+           best_helper(tree->right, tree->n + 1, max);
+}
+
+/**
+ * binary_tree_is_bst - Checks if is a valid BST
+ * @tree: A pointer to the root node
+ * Return: 1 if tree is a valid BST, and 0 otherwise
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+
+    return best_helper(tree, INT_MIN, INT_MAX);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <limits.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -58,5 +60,6 @@ void levelorder_help(const binary_tree_t *tree, void (*func)(int), int level);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_height2(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-
+bool best_helper(const binary_tree_t *tree, int min, int max);
+int binary_tree_is_bst(const binary_tree_t *tree);
 #endif
