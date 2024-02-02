6d8269c07833624afe85f55386cf2bd34170bd7c
“reemelfadilhassanin” <justreemjust49@gmail.com>
2024-02-02 06:36:03 +0400

    commit

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -9,49 +9,32 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-	{
+	size_t size;
+
+	if (!tree)
+		return (0);
+	size = binary_tree_size(tree);
+
+	return (helper_comple(tree, 0, size));
+}
+/**
+ * helper_comple - checks if a binary tree is complete
+ * @tree: a pointer to the root node of the tree to check
+ * @index: node index to check
+ * @size: number of nodes in the tree
+ *
+ * Return: 1 if the tree is complete
+ *         0 if the tree is not complete
+ *         0 if tree is NULL
+ */
+int bhelper_comple(const binary_tree_t *tree, size_t index, size_t size)
+{
+	if (!tree)
 		return (1);
-	}
 
-	int node_depth = 0;
-	binary_tree_t *current;
+	if (index >= size)
+		return (0);
 
-	current = (binary_tree_t *)tree;
-
-	while (current->left)
-	{
-		current = current->left;
-		node_depth++;
-	}
-
-	while (current)
-	{
-		if (current->left == NULL && current->right != NULL)
-		{
-			return (0);
-		}
-
-		for (int i = 0; i < (1 << (node_depth - 1)); i++)
-		{
-			if (current->right == NULL)
-			{
-				return (1);
-			}
-			current = current->right;
-		}
-
-
-		if (current->right->left)
-		{
-			current = current->right->left;
-		}
-		else
-		{
-			current = current->right;
-		}
-		node_depth--;
-	}
-
-	return (1);
+	return (bhelper_comple(tree->left, 2 * index + 1, size) &&
+			bhelper_comple(tree->right, 2 * index + 2, size));
 }
